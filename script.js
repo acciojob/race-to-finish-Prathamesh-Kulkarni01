9edfda5943d1d3912119f0fdc757e63bb65308c3
Prathamesh Kulkarni <65341679+Prathamesh-Kulkarni01@users.noreply.github.com>
2023-08-25 16:41:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 window.promises = [];
+let max=5,min=1;
+for(let i=0;i<6;i++){
+	const p=new Promise((res,rej)=>{
+		let time=Math.floor(Math.random()*(max-min+1)+min)
+		setTimeOut(()=>{
+			res()
+		},time*1000)
+	})
+	promises.push(p)
+}
+
+const output=document.getElementById("output")
+output.innerText=Promise.any(promises)
 
 // Do not change the code above this
 // add your promises to the array `promises`
